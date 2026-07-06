25e8c01b43195e142a47b67bd39dc100aafd2820
Sara <s.cristal.p@gmail.com>
2026-07-06 19:18:43 +0200

    install axios and because api is nested add data.data

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
-const [guests, setGuests] = useState([]);
-const [selectedGuestId, setSelectedGuestId] = useState([]);
-const [selectedGuest, setSelectedGuest] = useState([]);
+const API = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/COHORT_CODE/guests";
 
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      /* const response = await fetch("http://localhost:3000/guests");
-         const { data } = await response.json();*/
-      const {data} = await axios.get("http://localhost:3000/guests");
-      setGuests(data);
-    } catch (error) {
-      console.error("Error fetching guests:", error);
-    }
-  };
-
-  fetchData();
-}, []);
 
 export default function App() {
-  return <></>;
+  const [guests, setGuests] = useState([]);
+  const [selectedGuestId, setSelectedGuestId] = useState([]);
+  const [selectedGuest, setSelectedGuest] = useState([]);
+  
+  useEffect(() => {
+    const fetchGuests = async () => {
+      try {
+        /* const response = await fetch("http://localhost:3000/guests");
+           const { data } = await response.json();*/
+        const {data} = await axios.get(API);
+        setGuests(data.data);
+      } catch (error) {
+        console.error("Error fetching guests:", error);
+      }
+    };
+  
+    fetchGuests();
+  }, []);
+  return (
+  <main>
+  <div className="App">
+      <h1>Guest List</h1>
+      <ul>
+        {guests.map((guest) => (
+          <li key={guest.id}>
+            {guest.name} - {guest.email}
+          </li>
+        ))}
+      </ul>
+    </div>  
+  </main>
+  );
 }
