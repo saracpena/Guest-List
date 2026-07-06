7de2af2815c975d11a6bd0f0614165d76eda92c1
Sara <s.cristal.p@gmail.com>
2026-07-06 18:55:29 +0200

    opening statement code

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import { useState, useEffect } from "react";
+
+const [guests, setGuests] = useState([]);
+const [selectedGuestId, setSelectedGuestId] = useState([]);
+const [selectedGuest, setSelectedGuest] = useState([]);
+
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/guests");
+      const { data } = await response.json();
+      setGuests(data);
+    } catch (error) {
+      console.error("Error fetching guests:", error);
+    }
+  };
+
+  fetchData();
+}, []);
+
 export default function App() {
   return <></>;
 }
